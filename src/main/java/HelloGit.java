7d238d2720aee5af79c535944011c97bb74243bd
Liulong <1420197055@qq.com>
2022-04-28 07:48:39 +0800

    first commit

---
+++ src/main/java/HelloGit.java
@@ -0,0 +1,10 @@
+/**
+ * @author Liujianlong
+ * @description
+ * @create 2022-04-28 7:46
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("version1");
+    }
+}
