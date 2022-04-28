6b5b59f9946461c4a18c727dd1492beef7a607a8
Liulong <1420197055@qq.com>
2022-04-28 08:39:47 +0800

    no_confic_test branch

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -9,5 +9,6 @@ public class HelloGit {
         System.out.println("version2");
         System.out.println("version3");
         System.out.println("version-master_last-branch");
+        System.out.println("不冲突测试");
     }
 }
